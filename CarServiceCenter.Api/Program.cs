b45ea52ba4e5e46e93da158988de019652acc0c9
JamesBMH <92014572+JamesBMH@users.noreply.github.com>
2026-06-30 19:11:06 +0200

    chore(api): replace OpenAPI with Swashbuckle for net8.0 compatibility

--- CarServiceCenter.Api/Program.cs
+++ CarServiceCenter.Api/Program.cs
@@ -6,7 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -15,7 +16,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
